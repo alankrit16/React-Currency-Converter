aa2d64cea599f278e0c12d6a990f22e011a569ba
Alankrit Srivastava <alankrit.sri@gmail.com>
2021-03-31 16:46:52 +0530

    chart dynamic

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import {Line} from "react-chartjs-2";
+import endpoints from '../Utils/endpoints';
 
 const Chart = (props) => {
 
     const [state,setState] = useState({
         label:[],
         values:[],
+        error:""
     })
 
+    const formatDate = (date) => {
+        return date.toISOString().split("T")[0];
+    }
+
     useEffect(()=>{
+        async function getTrend(){
+            // currency=EUR&start=2020-09-01&end=2020-09-10
+            let end = new Date();
+            let start = new Date();
+            start.setDate(start.getDate()-60);
+            start = formatDate(start)
+            end = formatDate(end);
+            const url = `${endpoints.chartData}currency=${props.dropdownValue}&start=${start}&end=${end}`;
+            const response = await fetch(url,{
+                method:"GET"
+            });
 
-
-
-        const bpi = {"2013-09-01":97.0904,"2013-09-02":96.4906,"2013-09-03":96.8493,"2013-09-04":91.3508,"2013-09-05":91.8866,"2013-09-06":88.3304,"2013-09-07":90.3363,"2013-09-08":88.4926,"2013-09-09":90.6332,"2013-09-10":91.5685};
-        const destructure = Object.entries(bpi);
-        const label = []
-        const value = []
-        destructure.forEach(ele=>{
-            label.push(ele[0]);
-            value.push(ele[1])
-        })
-        setState(ps=>({...ps,label:label,values:value}))
+            const result = await response.json();
+            if(response.ok){
+            const bpi = result.bpi;
+            const destructure = Object.entries(bpi);
+            const label = []
+            const value = []
+            destructure.forEach(ele=>{
+                label.push(ele[0]);
+                value.push(ele[1])
+            })
+            setState(ps=>({...ps,label:label,values:value,error:""}))
+            }else{
+                setState(ps=>({...ps,error:"Something Went Wrong :("}))
+            }
+        }
+        getTrend()
     },[props.dropdownValue])
 
 
 
 
     return <div>
-        {props.dropdownValue}
-        <Line
+        {state.error===""?<Line
         height={400}
         width={800}
         data={{
@@ -42,7 +63,7 @@ const Chart = (props) => {
                 }
             ]
         }}
-        />
+        />:<h1>{state.error}</h1>}
     </div>
 }
 
--- src/Utils/endpoints.js
+++ src/Utils/endpoints.js
@@ -1,6 +1,6 @@
 const endpoints = {
     currentPrice: "https://api.coindesk.com/v1/bpi/currentprice.json",
-    
+    chartData: "https://api.coindesk.com/v1/bpi/historical/close.json?"
 }
 
 export default endpoints;
