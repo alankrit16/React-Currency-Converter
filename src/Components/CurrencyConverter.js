9be2ebc760336ee6aec792539144ea5c71fb7512
Alankrit Srivastava <alankrit.sri@gmail.com>
2021-03-31 16:27:19 +0530

    passing data

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chart from './Chart';
 import DropdownComponent from "./DropdownComponent"
 
 const CurrencyConverter = () => {
+
+    const [dropdownValue,setDropdownValue] = useState("")
+
+    const passValue = (fetchData) => {
+        setDropdownValue(fetchData)
+    }
+
     return <div class="row">
-    <div class="column"> <DropdownComponent/></div>
-    <div class="column"><Chart/></div>
+    <div class="column"> <DropdownComponent setDropdownValue={passValue}/></div>
+    {dropdownValue!==""?<div class="column"><Chart dropdownValue={dropdownValue} /></div>:null}
   </div>
 }
 
